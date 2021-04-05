7ba18531a910b59aaf440ac3d25f35143056d455
crayon <873217631@qq.com>
2021-04-05 22:23:13 +0800

    bugfix: redis锁不生效

--- djangocli/utils/redis.py
+++ djangocli/utils/redis.py
@@ -52,41 +52,37 @@ class RedisLock:
     redis_inst = RedisInstSingleTon.get_inst()
 
     def __init__(self, lock_name: str = None, lock_expire: int = DEFAULT_TIMEOUT):
-        self.lock_name = lock_name
+        self.lock_name = f"{REDIS_KEY_PREFIX.LOCK}:{lock_name}"
         self.lock_expire = lock_expire
+        self.identifier = None
 
     def __enter__(self):
         if self.lock_name is None:
             return None
-        self.identifier = self.acquire_lock_with_timeout(self.lock_name, self.lock_expire)
+        self.identifier = self.acquire_lock_with_timeout()
         return self.identifier
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.identifier is None:
             return False
-        self.release_lock(self.lock_name, self.identifier)
+        self.release_lock()
 
-    def acquire_lock_with_timeout(self, lock_name, lock_expire: int = DEFAULT_TIMEOUT):
+    def acquire_lock_with_timeout(self):
         """
         获得锁
-        :param lock_name: 锁名
-        :param lock_expire: 锁过期时间
         :return: success: identifier failed: None
         """
         identifier = str(uuid.uuid4())
-        lock_name = f"{REDIS_KEY_PREFIX.LOCK}:{lock_name}"
-        lock_timeout = int(math.ceil(lock_expire))
+        lock_timeout = int(math.ceil(self.lock_expire))
         # 如果不存在这个锁则加锁并设置过期时间，避免死锁
-        if self.redis_inst.set(lock_name, identifier, ex=lock_timeout, nx=True):
+        if self.redis_inst.set(self.lock_name, identifier, ex=lock_timeout, nx=True):
             return identifier
         # 锁已存在并被持有，此时放弃排队竞争，直接返回None
         return None
 
-    def release_lock(self, lock_name, identifier):
+    def release_lock(self):
         """
         释放锁
-        :param lock_name: 锁的名称
-        :param identifier: 锁的标识
         :return:
         """
         unlock_script = """
@@ -96,7 +92,6 @@ class RedisLock:
             return 0
         end
         """
-        lock_name = f"lock:{lock_name}"
         unlock = self.redis_inst.register_script(unlock_script)
-        result = unlock(keys=[lock_name], args=[identifier])
+        result = unlock(keys=[self.lock_name], args=[self.identifier])
         return result
